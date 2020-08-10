c3b100efc6b3b0987736e3a7fa93dd7f31319dca
Joel Dahne <joel@dahne.eu>
2020-08-10 15:46:37 +0200

    Improve computation of T0
    Add more tuning parameters and also add computation of T022.

--- src/FractionalKdV/T0.jl
+++ src/FractionalKdV/T0.jl
@@ -4,13 +4,17 @@ Returns a function such that T0(u0)(x) is the function whose supremum
 on [0, π] gives C_B. The strategy for evaluation depends on type of
 evaltype.
 """
-T0(u0::FractionalKdVAnsatz) = T0(u0, Ball())
+T0(u0::FractionalKdVAnsatz; kwargs...) = T0(u0, Ball(); kwargs...)
 
-function T0(u0::FractionalKdVAnsatz{arb}, evaltype::Ball)
+function T0(u0::FractionalKdVAnsatz{arb},
+            evaltype::Ball;
+            rtol = -1.0,
+            )
+    δ0 = parent(u0.α)("1e-6")
+    δ1 = parent(u0.α)("1e-6")
+    δ2 = parent(u0.α)("1e-6")
+
     return x -> begin
-        δ0 = parent(u0.α)("1e-6")
-        δ1 = parent(u0.α)("1e-6")
-
         ## Integral on [0, x] - Change to t = y/x
 
         # Integral on [0, δ0]
@@ -23,15 +27,20 @@ function T0(u0::FractionalKdVAnsatz{arb}, evaltype::Ball)
 
         T01 = (
             T011
-            + T012(u0, δ0 = δ0, δ1 = δ1)(x)
+            + T012(u0, δ0 = δ0, δ1 = δ1, rtol = rtol)(x)
             + T013
         )
 
         ## Integral on [x, π]
-        # TODO: Implement this
-        T02 = zero(u0.α)
 
-        T02 /= parent(u0.α)(π)*u0.w(x)*u0(x)
+        # Integral on [x, x + δ2]
+        # TODO: Implement this
+        T021 = zero(u0.α)/(parent(u0.α)(π)*u0.w(x)*u0(x))
+
+        T02 = (
+            T021
+            + T022(u0, δ2 = δ2, rtol = rtol)(x)
+        )
 
         return T01 + T02
     end
@@ -53,12 +62,16 @@ T_{0,1,2} from the paper.
 T012(u0::FractionalKdVAnsatz;
      δ0::arb = parent(u0.α)("1e-6"),
      δ1::arb = parent(u0.α)("1e-6"),
-     ) = T012(u0, Ball(), δ0 = δ0, δ1 = δ1)
+     rtol = -1.0,
+     atol = -1.0,
+     ) = T012(u0, Ball(), δ0 = δ0, δ1 = δ1, rtol = rtol, atol = atol)
 
 function T012(u0::FractionalKdVAnsatz{arb},
               evaltype::Ball;
               δ0::arb = parent(u0.α)("1e-6"),
               δ1::arb = parent(u0.α)("1e-6"),
+              rtol = -1.0,
+              atol = -1.0,
               )
     return x -> begin
         CC = ComplexField(prec(parent(u0.α)))
@@ -73,12 +86,60 @@ function T012(u0::FractionalKdVAnsatz{arb},
             analytic = analytic,
         )*t^u0.p
         res = real(ArbTools.integrate(CC, F, a, b,
-                                      # TODO: Fix the bug that makes this not work
-                                      #rel_tol = 1e-4,
+                                      rel_tol = rtol,
+                                      abs_tol = atol,
                                       eval_limit = 3000,
                                       verbose = 1,
                                       ))
 
         return res*x/(parent(u0.α)(π)*u0(x))
+    end
+end
+
+"""
+    T022(u0::FractionalKdVAnsatz;
+Returns a function such that T022(u0, δ0 = δ0)(x) computes the (not yet
+existing) integral T_{0,2,2} from the paper. That is, the integral
+from x + δ0 to π. If x + δ0 >= π the function returns zero, note that
+the integral T02 might be far from zero in this case.
+)
+"""
+T022(u0::FractionalKdVAnsatz;
+     δ2::arb = parent(u0.α)("1e-6"),
+     rtol = -1.0,
+     atol = -1.0,
+     ) = T022(u0, Ball(), δ2 = δ2, rtol = rtol, atol = atol)
+
+function T022(u0::FractionalKdVAnsatz{arb},
+              evaltype::Ball;
+              δ2::arb = parent(u0.α)("1e-6"),
+              rtol = -1.0,
+              atol = -1.0,
+              )
+    return x -> begin
+        if x + δ2 >= parent(u0.α)(π)
+            @warn "Integral T022 is evaluated on an empty interval"
+            return zero(u0.α)
+        end
+
+        CC = ComplexField(prec(parent(u0.α)))
+        mα = CC(-u0.α)
+        a = CC(x + δ2)
+        b = CC(parent(u0.α)(π))
+
+        # PROVE: That there are no branch cuts that interact with the
+        # integral
+        F(y, analytic = false) = ArbTools.real_abs(
+            Ci(x - y, mα) + Ci(x + y, mα) - 2Ci(y, mα),
+            analytic = analytic,
+        )*y^u0.p
+        res = real(ArbTools.integrate(CC, F, a, b,
+                                      rel_tol = rtol,
+                                      abs_tol = atol,
+                                      eval_limit = 2000,
+                                      verbose = 1,
+                                      ))
+
+        return res/(parent(u0.α)(π)*u0.w(x)*u0(x))
     end
 end
