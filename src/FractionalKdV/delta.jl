86e41ac7521c48d000e58ebae6b7d649a4c3e7ed
Joel Dahne <joel@dahne.eu>
2020-09-15 20:19:53 +0200

    Update delta0

--- src/FractionalKdV/delta.jl
+++ src/FractionalKdV/delta.jl
@@ -1,35 +1,68 @@
 export delta0, delta0_bounded_by, delta0_estimate
 
 """
-    delta0(u0::FractionalKdVAnsatz; ϵ::arb = parent(u0.α)(0.1))
-Upper bound the value of δ₀ from the paper. Uses an asymptotic
-expansion on the interval [0, ϵ] and ball arithmetic on [ϵ, π].
+    delta0(u0::FractionalKdVAnsatz; ϵ::arb = 0, M::Integer = 3, n::Integer = 6)
+Upper bound the value of `δ₀` from the paper.
 
-TODO: This will likely have to be optimised for when α is a ball. One
-of the main problems at the moment is that it doesn't notice when the
-result can't be improved due to overestimation coming from α.
+Uses an asymptotic expansion with `M` terms on the interval `[0, ϵ]`
+and ball arithmetic together with Taylor expansions of degree `n` on
+`[ϵ, π]`.
+
+If `ϵ` is zero then it tries to determine an optimal value of it by
+finding where the asymptotic expansion gives better bounds than ball
+arithmetic. This is mostly relevant when `u0.α` is wide.
+
+TODO: Look more at tuning for `n`, it seems to depend on the precise
+values. It might be beneficial to reduce `ϵ` in smaller steps. It's
+mainly the `[ϵ, π]` that takes time, could look more at what part of
+it. Otherwise it's mainly important to look at the overestimation in
+the evaluation due to the radius of `α`.
 """
 function delta0(u0::FractionalKdVAnsatz{arb};
-                ϵ::arb = parent(u0.α)(1e-3),
+                ϵ::arb = zero(u0.α),
                 M::Integer = 3,
-                rtol::arb = parent(u0.α)(1e-4),
+                n::Integer = 6,
+                rtol::arb = parent(u0.α)(1e-3),
                 show_trace = false,
                 )
+    f = F0(u0, Asymptotic(), M = M)
+    g = F0(u0)
+    if iszero(ϵ)
+        # Find a value of ϵ such that the asymptotic error is smaller
+        # than the direct one
+        ϵ = one(ϵ)
+        while !(radius(f(ϵ)) < radius(g(ϵ))) && ϵ > 1e-3
+            ϵ /= 2
+        end
+    end
+
     # Bound the value one [0, ϵ]
-    res1 = enclosemaximum(F0(u0, Asymptotic(), M = M), parent(u0.α)(0), ϵ,
+    # Estimate the value by evaluating it at ϵ
+    estimate = abs(f(ϵ))
+    res1 = enclosemaximum(f, parent(u0.α)(0), ϵ,
+                          lower_bound = estimate,
                           rtol = rtol,
+                          atol = 2radius(estimate),
                           absmax = true,
-                          maxevals = 10^3,
+                          maxevals = 10^4,
                           show_trace = show_trace,
                           )
 
     # Bound the value on [ϵ, π] by Ball evaluation
-    res2 = enclosemaximum(F0(u0), ϵ, parent(u0.α)(π),
+    # Estimate the value by evaluating it at a number of points on the interval
+    xs = ϵ .+ (parent(ϵ)(π) - ϵ).*range(0, stop = 1, length = 10)
+    estimate = zero(ϵ)
+    for x in xs
+        estimate = max(estimate, abs(g(x)))
+    end
+    res2 = enclosemaximum(g, ϵ, parent(u0.α)(π),
                           evaltype = :taylor,
-                          n = 8,
+                          n = n,
+                          lower_bound = estimate,
                           rtol = rtol,
+                          atol = 4radius(estimate),
                           absmax = true,
-                          maxevals = 10^3,
+                          maxevals = 10^4,
                           show_trace = show_trace,
                           )
 
@@ -78,7 +111,7 @@ If `return_values = true` then also return the points and the computed
 values.
 """
 function delta0_estimate(u0::FractionalKdVAnsatz{T};
-                         ϵ = 0.1,
+                         ϵ = 0,
                          M::Integer = 3,
                          n::Integer = 100,
                          return_values = false,
