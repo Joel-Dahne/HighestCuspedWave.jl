46d726086c58d93e7bea84d360444ad028ccd99d
Joel Dahne <joel@dahne.eu>
2020-10-23 15:40:23 +0200

    Add asymptotic versions of u0, H and D for BHAnsatz

--- src/BurgersHilbert/evaluation.jl
+++ src/BurgersHilbert/evaluation.jl
@@ -8,6 +8,55 @@ function (u0::BHAnsatz)(x, ::Ball)
     return res
 end
 
+function (u0::BHAnsatz{arb})(x, ::Asymptotic; M::Integer = 3)
+    expansion = u0(x, AsymptoticExpansion(), M = M)
+
+    res = zero(x)
+    for ((i, m), a) in expansion
+        if i > 0
+            res += a*log(abs(x))^i*abspow(x, m)
+        else
+            res += a*abspow(x, m)
+        end
+    end
+
+    return res
+end
+
+"""
+    (u0::AbstractAnsatz)(x, ::AsymptoticExpansion; M = 3)
+Return a dictionary containing the terms in the asymptotic expansion
+of `u0`. The element `((i, m), a)` in the dictionary corresponds to
+the term `a*log(abs(x))^i*abs(x)^m`. The highest term is an error term
+which makes sure that evaluation of the expansion gives an enclosure
+of the result.
+"""
+function (u0::BHAnsatz{arb})(x, ::AsymptoticExpansion; M::Integer = 3)
+    # TODO: Check this
+    #@assert M >= 3
+
+    RR = parent(u0.a0)
+    π = RR(pi)
+    γ = RR(stieltjes(arb, 0))
+
+    res = Dict{NTuple{2, Int}, arb}()
+
+    res[(1, 1)] = -u0.a0*π/2
+    res[(0, 1)] = -u0.a0*(γ - 1)*π/2
+
+    for m in 1:M-1
+        term = u0.a0*zeta(RR(2 - 2m), d = 1)
+        for n in 1:u0.N
+            term += fmpz(n)^(2m)*u0.b[n]
+        end
+        res[(0, 2m)] = (-1)^m*term/factorial(fmpz(2m))
+    end
+
+    #TODO: Add error term
+
+    return res
+end
+
 function H(u0::BHAnsatz, ::Ball)
     return x -> begin
         res = -u0.a0*(Ci(x, 3, 1) - zeta(3, d = 1))
@@ -19,6 +68,114 @@ function H(u0::BHAnsatz, ::Ball)
         return res
     end
 end
+
+function H(u0::BHAnsatz, ::Asymptotic; M::Integer = 3)
+    f = H(u0, AsymptoticExpansion(), M = M)
+
+    return x -> begin
+        expansion = f(x)
+
+        res = zero(x)
+        for ((i, m), a) in expansion
+            if i > 0
+                res += a*log(abs(x))^i*abspow(x, m)
+            else
+                res += a*abspow(x, m)
+            end
+        end
+
+        return res
+    end
+end
+
+"""
+    H(u0::AbstractAnsatz, ::AsymptoticExpansion; M = 3)
+Returns a function such that `H(u0, AsymptoticExpansion)(x)` returns a
+dictionary containing the terms in the asymptotic expansion of
+`H(u0)`. The element `((i, m), a)` in the dictionary corresponds to
+the term `a*log(abs(x))^i*abs(x)^m`. The highest term is an error term
+which makes sure that evaluation of the expansion gives an enclosure
+of the result.
+"""
+function H(u0::BHAnsatz{arb}, ::AsymptoticExpansion; M::Integer = 3)
+    # TODO: Check this
+    #@assert M >= 3
+
+    RR = parent(u0.a0)
+    π = RR(pi)
+    γ = RR(stieltjes(arb, 0))
+    γ₁ = RR(stieltjes(arb, 1))
+
+    return x -> begin
+        res = Dict{NTuple{2, Int}, arb}()
+
+        res[(2, 2)] = -u0.a0/4
+        res[(1, 2)] = u0.a0*(3//4 - γ/2)
+
+        for m in 1:M - 1
+            if m == 1
+                term = -u0.a0*(3γ - γ^2 - 2γ₁ - 7//2 + (π^2)/12)/2
+            else
+                term = -u0.a0*zeta(3 - 2m, d = 1)
+            end
+            for n in 1:u0.N
+                term -= fmpz(n)^(2m - 1)*u0.b[n]
+            end
+            res[(0, 2m)] = (-1)^m*term/factorial(fmpz(2m))
+        end
+
+        # TODO: Add error term
+
+        return res
+    end
+end
+
+function D(u0::BHAnsatz, ::Asymptotic; M::Integer = 3)
+    f = D(u0, AsymptoticExpansion(), M = M)
+
+    return x -> begin
+        expansion = f(x)
+
+        res = zero(x)
+        for ((i, m), a) in expansion
+            if i > 0
+                res += a*log(abs(x))^i*abspow(x, m)
+            else
+                res += a*abspow(x, m)
+            end
+        end
+
+        return res
+    end
+end
+
+function D(u0::BHAnsatz{arb}, evaltype::AsymptoticExpansion; M::Integer = 3)
+    f = x -> u0(x, evaltype, M = M)
+    g = H(u0, evaltype, M = M)
+
+    return x -> begin
+        expansion = Dict{NTuple{2, Int}, arb}()
+
+        expansion1 = f(x)
+        for ((i1, m1), a1) in expansion1
+            for ((i2, m2), a2) in expansion1
+                key = (i1 + i2, m1 + m2)
+                expansion[key] = get(expansion, key, zero(x)) + a1*a2/2
+            end
+        end
+
+        expansion2 = g(x)
+        for (key, a) in expansion2
+            expansion[key] = get(expansion, key, zero(x)) + a
+        end
+
+        # TODO: Will have to handle terms which are supposed to be
+        # identically equal to zero.
+
+        return expansion
+    end
+end
+
 
 """
     D(u0::BHAnsatz, xs::AbstractVector)
