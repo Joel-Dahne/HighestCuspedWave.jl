5dbd9e8d29126a76123727956df34fbe2efa6b8b
Joel Dahne <joel@dahne.eu>
2021-08-12 16:36:42 +0200

    Update runtests
    Set the precision to use for Arb and skip some tests which are not
    done and very slow.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,16 @@ using Nemo
 using ArbTools
 using Arblib
 
-@testset "HighestCuspedWave" begin
-    include("arb-test.jl")
-    include("clausian-test.jl")
-    include("evaluation-test.jl")
+setprecision(Arb, 64) do
+    @testset "HighestCuspedWave" begin
+        include("arb-test.jl")
+        include("clausian-test.jl")
+        include("evaluation-test.jl")
 
-    include("T0-test.jl")
+        #include("T0-test.jl") # Not finished and takes a long time
 
-    include("alpha-test.jl")
-    include("delta-test.jl")
-    #include("CB-test.jl")
+        include("alpha-test.jl")
+        include("delta-test.jl")
+        #include("CB-test.jl")
+    end
 end
