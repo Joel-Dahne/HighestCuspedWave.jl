0ccabb07e56bd2955b95cd86ac159b6cc8cc9d55
Joel Dahne <joel@dahne.eu>
2021-09-29 18:54:21 -0400

    Clean up determination methods for BH

--- src/BurgersHilbert/determination.jl
+++ src/BurgersHilbert/determination.jl
@@ -1,28 +1,27 @@
 """
     findbs!(u0::BHAnsatz)
-Find values of b[n] to minimize the defect D(u0).
+Find values of `b[n]` to minimize the defect `D(u0)`.
 
 This is done by solving the non-linear system given by requiring that
-D(u0) evaluates to zero on N collocation points.
+`D(u0)` evaluates to zero on `N` collocation points.
 
-It uses nlsolve to find the zero, however nlsolve doesn't support Arb
-so this is always done in Float64.
+For performance reasons this is always done in `Float64`
 """
-function findbs!(u0::BHAnsatz{T}) where {T}
-    if u0.N == 0
-        return u0
-    end
+function findbs!(u0::BHAnsatz{Float64})
+    u0.N == 0 && return u0
 
     n = u0.N
     xs = π * (1:2:2n-1) / 2n
 
+    # Function for computing D(u0) on the points xs. It takes the
+    # coefficients b[n] as an argument
     f = D(u0, xs)
 
     # The initial value we use depends on if u0.v0 is set or not
     if isnothing(u0.v0)
         # Initial values which originally came from following the branch
         # but are now taken as given.
-        initial = T[
+        initial = [
             -0.5332599597604509
             -0.12693649125903272
             -0.05148250768408276
@@ -34,28 +33,25 @@ function findbs!(u0::BHAnsatz{T}) where {T}
         ]
     else
         # In this case we start zero as the initial guess
-        initial = T[]
+        initial = zeros()
     end
-    initial = [initial; zeros(T, max(n - length(initial), 0))][1:n]
+    initial = [initial; zeros(max(n - length(initial), 0))][1:n]
 
     sol = nlsolve(f, initial, autodiff = :forward)
 
-    if !sol.f_converged
-        @warn "Solution did not converge for u0.N = $n"
-        @warn sol
-    end
+    sol.f_converged || @warn "Solution did not converge for u0.N = $n" sol
 
     copy!(u0.b, sol.zero)
 
     return u0
 end
 
-function findbs!(u0::BHAnsatz{Arb})
+function findbs!(u0::BHAnsatz)
     u0_float = convert(BHAnsatz{Float64}, u0)
 
     findbs!(u0_float)
 
-    u0.b .= u0_float.b
+    copy!(u0.b, u0_float.b)
 
     return u0
 end
