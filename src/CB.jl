24d429f34503a1fc73d28e86d011671ec090b3c4
Joel Dahne <joel@dahne.eu>
2021-10-07 10:26:14 -0400

    Update CB_estimate
    It no longer supports Nemo. It has a new option for enabling or
    disabling threading.

--- src/CB.jl
+++ src/CB.jl
@@ -20,13 +20,20 @@ CB_bounded_by
 
 
 """
-    CB_estimate(u0::FractionalKdVAnsatz; n::Integer = 100)
-Estimate the value of C_B from the paper. Does this by evaluating the
-norm of it on n linearly spaced points. This always gives a lower bound
-of C_B. Currently it gives a very bad estimate.
+    CB_estimate(u0::FractionalKdVAnsatz; n::Integer = 20)
+
+Estimate the value of `C_B` from the paper. Does this by evaluating
+the norm of it on `n` linearly spaced points. This always gives a
+lower bound of `C_B`.
+
+Notice that the default value for `n` is rather low since evaluation
+of the integral is costly. Therefore it gives a rather poor estimate
+of the norm.
 
 If `return_values = true` then also return the points and the computed
 values.
+
+If `threaded = true` computes the evaluations in parallel.
 
 TODO: Use the asymptotic version close to zero.
 """
@@ -34,28 +41,24 @@ function CB_estimate(
     u0::AbstractAnsatz{T};
     n::Integer = 20,
     return_values = false,
-    show_trace = false,
+    threaded = true,
 ) where {T}
-    xs = range(0, stop = π, length = n)[2:end]
-    if T == arb
-        xs = u0.parent.(xs)
-    else
-        xs = convert.(T, xs)
-    end
-
+    xs = range(zero(T), π, length = n + 1)[2:end]
     res = similar(xs)
-    f = T0(u0, Ball(), rtol = 1e-6, atol = 1e-6; show_trace)
-    Threads.@threads for i in eachindex(xs)
-        res[i] = f(xs[i])
+
+    f = T0(u0, Ball())
+    if threaded
+        Threads.@threads for i in eachindex(xs)
+            res[i] = f(xs[i])
+        end
+    else
+        for i in eachindex(xs)
+            res[i] = f(xs[i])
+        end
     end
 
-    m = zero(xs[1])
-    for r in res
-        m = max(m, abs(r))
-    end
+    m = maximum(abs.(res))
 
-    if return_values
-        return m, xs, res
-    end
+    return_values && return m, xs, res
     return m
 end
