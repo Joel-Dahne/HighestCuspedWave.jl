fd35472a1dbfc2ceb724c02b652a98d9332d2a96
Joel Dahne <joel@dahne.eu>
2021-10-07 10:14:41 -0400

    Update alpha0 for FractionalKdVAnsatz
    Remove the Nemo version and updated the Arblib version with threading
    and verbose options.

--- src/FractionalKdV/alpha.jl
+++ src/FractionalKdV/alpha.jl
@@ -1,83 +1,49 @@
 """
-    alpha0(u0::FractionalKdVAnsatz; M::Integer = 3, rtol::arb = 1e-5)
-Upper bound the value of `α₀` from the paper.
+    alpha0(u0::FractionalKdVAnsatz; M::Integer = 3, rtol::arb = 1e-5, threaded = true)
+
+Enclose the value of `α₀` from the paper.
 
 It uses an asymptotic expansion with `M` terms close to zero and ball
 arithmetic on the remaining.
 
 In practice the maximum is attained at `x = π` so the function is
 evaluated there first. Then starting at `ϵ = π/2` we halve `ϵ` until
-the bound on `[0, ϵ]` is smaller than the value at `π`. On the
-interval `[ϵ, π]` bounded by evaluation and bisection. Notice that we
-do not have to prove that the maximum is attained at `π`, it's only
-used to make the procedure more efficient.
+the bound on `[0, ϵ]` is smaller than the value at `π`.
+
+On the interval `[ϵ, π]` we bound it using ball arithmetic with
+[`ArbExtras.maximum_enclosure`](@ref). Notice that we do not have to
+prove that the maximum is attained at `x = π`, it's only used to make
+the procedure more efficient.
+
+If `threaded = true` it enables threading when calling
+[`ArbExtras.maximum_enclosure`](@ref). If `verbose = true` print more
+information about the process.
 """
 function alpha0(
-    u0::FractionalKdVAnsatz{arb};
+    u0::FractionalKdVAnsatz{Arb};
     M::Integer = 3,
-    rtol::arb = parent(u0.α)(1e-5),
+    rtol = 1e-5,
+    threaded = true,
+    verbose = false,
 )
     # This is required for a finite value
     @assert u0.p + u0.α > 0
 
-    f(x) = begin
-        u0.w(x) / (2u0(x))
-    end
-
-    # The maximum is in practice attained at x = π, so evaluate at this point
-    m1 = f(parent(u0.α)(π))
-
-    ϵ = parent(u0.α)(π) / 2
-    # Make sure the asymptotic values is well defined
-    while isnan(c(u0, ϵ, M = M))
-        ϵ /= 2
-    end
-    # Continue halving ϵ until the asymptotic value is smaller
-    # than m1, but don't go to far. We have
-    # abs(x)^p/u0(x) = (1 + hat(u0)(x))/a0(u0, 0)*abs(x)^(u0.p + u0.α)
-    # <= (1 + c(u0, ϵ)*abs(x)^u0.p0/a0(u0, 0)*abs(x)^(u0.p + u0.α)
-    # Which attains its maximum value at x = ϵ
-    m2 = (1 + c(u0, ϵ) * abs(ϵ)^u0.p0) / a0(u0, 0) * abs(ϵ)^(u0.p + u0.α)
-    while !(m2 < m1) && ϵ > 1e-10
-        ϵ /= 2
-        m2 = (1 + c(u0, ϵ) * abs(ϵ)^u0.p0) / a0(u0, 0) * abs(ϵ)^(u0.p + u0.α)
-    end
-
-    if m2 > m1
-        @warn "We have m1 > m2 which should not happen in practice"
-    end
-
-    # Bound the value on [ϵ, π] by Ball evaluation
-    m3 = enclosemaximum(
-        f,
-        ϵ,
-        parent(u0.α)(π),
-        lower_bound = m1, # m1 is a lower bound of the maximum
-        rtol = rtol,
-        atol = 2radius(m1), # We cannot expect to do better than m1
-        absmax = true,
-    )
-
-    return max(m1, m2, m3)
-end
-
-function alpha0(u0::FractionalKdVAnsatz{Arb}; M::Integer = 3, rtol = 1e-5)
-    # This is required for a finite value
-    @assert u0.p + u0.α > 0
-
     # This is the function we want to find the maximum of
-    f(x) = begin
-        u0.w(x) / (2u0(x))
-    end
+    f(x) = u0.w(x) / (2u0(x))
 
     # The maximum is in practice attained at x = π, so evaluate at this point
     m1 = f(Arb(π))
 
     ϵ = Arb(π) / 2
-    # Make sure the asymptotic values is well defined
+    # Make sure ϵ is small enough so that the asymptotic value is well
+    # defined
     while isnan(c(u0, ϵ; M))
         ϵ /= 2
     end
+
+    verbose && @info "ϵ value for finite bound" ϵ
+
     # Continue halving ϵ until the asymptotic value is smaller
     # than m1, but don't go to far. We have
     # abs(x)^p/u0(x) = (1 + hat(u0)(x))/a0(u0, 0)*abs(x)^(u0.p + u0.α)
@@ -89,9 +55,9 @@ function alpha0(u0::FractionalKdVAnsatz{Arb}; M::Integer = 3, rtol = 1e-5)
         m2 = (1 + c(u0, ϵ) * abs(ϵ)^u0.p0) / a0(u0, 0) * abs(ϵ)^(u0.p + u0.α)
     end
 
-    if m2 > m1
-        @warn "We have m1 > m2 which should not happen in practice"
-    end
+    verbose && @info "ϵ value for getting smaller value than at π" ϵ
+
+    m2 > m1 && @warn "We have m1 > m2 which should not happen in practice"
 
     # Bound the value on [ϵ, π] by Ball evaluation
     m3 = ArbExtras.maximum_enclosure(
@@ -99,10 +65,11 @@ function alpha0(u0::FractionalKdVAnsatz{Arb}; M::Integer = 3, rtol = 1e-5)
         Arblib.lbound(ϵ),
         Arblib.ubound(Arb(π)),
         abs_value = true,
-        threaded = true;
         point_value_max = m1, # m1 is a lower bound of the maximum
-        atol = 2Arblib.radius(m1), # We cannot expect to do better than m1
+        atol = 4Arblib.radius(m1); # We cannot expect to do better than m1
         rtol,
+        threaded,
+        verbose,
     )
 
     return max(m1, m2, m3)
