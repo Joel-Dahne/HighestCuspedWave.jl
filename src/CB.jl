de69acb4f0754f32c375cc3c2180bb2c4df59524
Joel Dahne <joel@dahne.eu>
2021-10-14 19:56:47 -0400

    Add support for adding an error to x for CB_estimate

--- src/CB.jl
+++ src/CB.jl
@@ -20,7 +20,7 @@ CB_bounded_by
 
 
 """
-    CB_estimate(u0::FractionalKdVAnsatz; n::Integer = 20)
+    CB_estimate(u0::FractionalKdVAnsatz; n::Integer = 20, add_error = 0)
 
 Estimate the value of `C_B` from the paper. Does this by evaluating
 the norm of it on `n` linearly spaced points. This always gives a
@@ -33,6 +33,10 @@ of the norm.
 If `return_values = true` then also return the points and the computed
 values.
 
+If `x_error` is non-zero and the type used is `Arb` then add the error
+`x_error` to each value of `x`. This is useful if you want to assure
+that the bounds holds even if the value for `x` is not a tight ball.
+
 If `threaded = true` computes the evaluations in parallel.
 
 TODO: Use the asymptotic version close to zero.
@@ -41,9 +45,16 @@ function CB_estimate(
     u0::AbstractAnsatz{T};
     n::Integer = 20,
     return_values = false,
+    x_error = zero(T),
     threaded = true,
 ) where {T}
     xs = range(zero(T), π, length = n + 1)[2:end]
+
+    if !iszero(x_error)
+        @assert T == Arb
+        xs = Arblib.add_error!.(xs, x_error)
+    end
+
     res = similar(xs)
 
     f = T0(u0, Ball())
