db5c02ab301b40ecaabd8b92fa2a7c106f2e0b28
Joel Dahne <joel@dahne.eu>
2021-10-28 09:57:07 -0400

    Cleanup determination for KdV
    Remove Nemo methods, update some documentation and remove some old
    todos.

--- src/FractionalKdV/determination.jl
+++ src/FractionalKdV/determination.jl
@@ -1,10 +1,13 @@
 """
     findp0(α)
-Compute the smallest p0 ∈ (0, ∞) such that
+
+Compute the smallest `p0 ∈ (0, ∞)` such that
+```
 cos(π(2α - p0)/2)*Γ(2α -p0)/(cos(π(α - p0)/2)) - Γ(1 + 2α)cos(πα)/(α*Γ(α)cos(πα/2))
+```
 is equal to zero.
 
-We use, but don't have to prove, that p0 < 1.5(α + 1).
+We use, but don't have to prove, that `p0 < 1.5(α + 1)`.
 """
 function findp0(α)
     Γ = SpecialFunctions.gamma
@@ -21,42 +24,6 @@ function findp0(α)
     i = findfirst(i -> res[i] * res[i+1] <= 0, 1:n-1)
 
     p0 = first(nlsolve(p -> [f(p[1])], [ps[i]], autodiff = :forward, ftol = 1e-15).zero)
-
-    return p0
-end
-
-function findp0(α::arb)
-    # Do the computations at a higher precision
-    if iswide(α)
-        # PROVE: That p0 is monotone in α
-        α_low, α_upp = getinterval(α)
-        return ArbTools.setunion(findp0(α_low), findp0(α_upp))
-    end
-    α = RealField(2precision(parent(α)))(α)
-    Γ = Nemo.gamma
-    C = 2Γ(2α)cospi(α) / (Γ(α)cospi(α / 2))
-    f(p) = begin
-        cospi((2α - p) / 2) * Γ(2α - p) / (cospi((α - p) / 2) * Γ(α - p)) - C
-    end
-
-    # PROVE: That it is the smallest positive zero
-    p0 = ArbTools.add_error!(parent(α)(findp0(Float64(α))), parent(α)(1e-12))
-
-    found, flags = isolateroots(
-        f,
-        getinterval(p0)...,
-        atol = 1e-15,
-        rtol = 1e-15,
-        maxevals = 10000,
-        evaltype = :taylor,
-        refine = true,
-        maxfound = 1,
-    )
-
-    @assert only(flags)
-    p0 = setunion(only(found)...)
-
-    p0 = RealField(div(precision(parent(α)), 2))(p0)
 
     return p0
 end
@@ -112,12 +79,16 @@ end
 
 """
     finda0(α)
-Compute a[0] such that a0(u0, 0)^2/2 - A0(u0, 0) is zero. That is,
-compute a[0] = 2Γ(2α)*sinpi((1 - 2α)/2)/(Γ(α)^2*sinpi((1 - α)/2)^2).
+
+Compute `a[0]` such that `a0(u0, 0)^2/2 - A0(u0, 0)` is zero. That is,
+compute
+```
+a[0] = 2Γ(2α)*sinpi((1 - 2α)/2)/(Γ(α)^2*sinpi((1 - α)/2)^2)
+```
 It makes use of the monotinicity to get good enclosures for wide
 balls.
 
-PROVE: That a[0] is monotone in α
+**PROVE:** That `a[0]` is monotone in `α`.
 """
 function finda0(α)
     if iswide(α)
@@ -126,15 +97,6 @@ function finda0(α)
         return Arb((finda0(α_low), finda0(α_upp)))
     end
     Γ = SpecialFunctions.gamma
-    return 2Γ(2α) * cospi(α) / (Γ(α)^2 * cospi(α / 2)^2)
-end
-
-function finda0(α::arb)
-    if iswide(α)
-        α_low, α_upp = getinterval(α)
-        return ArbTools.setunion(finda0(α_low), finda0(α_upp))
-    end
-    Γ = Nemo.gamma
     return 2Γ(2α) * cospi(α) / (Γ(α)^2 * cospi(α / 2)^2)
 end
 
@@ -157,15 +119,16 @@ end
 
 """
     findas(u0)
-Find the ones of a[j] for j > 0 that makes the coefficients of the
-leading terms in the asymptotic expansion zero.
+
+Find `a[j]` for `j > 0` that makes the coefficients of the leading
+terms in the asymptotic expansion of the defect zero.
 
 This is done by solving the corresponding non-linear system.
 
-It uses nlsolve to find the zero, however nlsolve doesn't support
-arb-types so this is always done in Float64. To speed it up we start
-by computing the first `n` coefficients, then `2n` and so on until we
-reach `u0.N0`.
+It uses [`nlsolve`](@ref) to find the zero, however `nlsolve` doesn't
+support `Arb` so this is always done in `Float64`. To speed it up we
+start by computing the first `n` coefficients, then `2n` and so on
+until we reach `u0.N0`.
 """
 function findas(u0::FractionalKdVAnsatz{T}; minstart = 16) where {T}
     if iszero(u0.N0)
@@ -189,10 +152,6 @@ function findas(u0::FractionalKdVAnsatz{T}; minstart = 16) where {T}
     end
 
     return u0.a[1:end]
-end
-
-function findas(u0::FractionalKdVAnsatz{arb})
-    return parent(u0.α).(findas(convert(FractionalKdVAnsatz{Float64}, u0)))
 end
 
 function findas(u0::FractionalKdVAnsatz{Arb})
@@ -221,9 +180,7 @@ function findas!(u0::FractionalKdVAnsatz{T}; use_midpoint = true) where {T}
             # The choice of p0 makes also the term (2, 1, 0), given by a0(u0,
             # 0)a0(u0, 1) - A0(u0, 0), equal to zero.
             if !use_midpoint
-                if T == arb
-                    @assert contains_zero(a0(u0, 0)a0(u0, 1) - A0(u0, 1))
-                elseif T == Arb
+                if T == Arb
                     @assert Arblib.contains_zero(a0(u0, 0)a0(u0, 1) - A0(u0, 1))
                 else
                     @assert a0(u0, 0)a0(u0, 1) - A0(u0, 1) ≈ 0.0
@@ -259,16 +216,14 @@ end
 
 """
     findbs!(u0)
-Find values of b[n] to minimize the defect D(u0).
+
+Find values of `b[n]` to minimize the defect `D(u0)`.
 
 This is done by solving the non-linear system given by requiring that
-D(u0) evaluates to zero on N1 collocation points.
+`D(u0)` evaluates to zero on `N1` collocation points.
 
-It uses nlsolve to find the zero, however nlsolve doesn't support
-arb-types so this is always done in Float64.
-
-TODO: Possibly add support for optimizing some of the the a[j]'s at
-the same time,
+It uses [`nlsolve`](@ref) to find the zero, however `nlsolve` doesn't
+support `Arb` so this is always done in `Float64`.
 """
 function findbs!(u0::FractionalKdVAnsatz)
     if u0.N1 == 0
@@ -302,24 +257,10 @@ function findbs!(u0::FractionalKdVAnsatz)
     return u0
 end
 
-function findbs!(u0::FractionalKdVAnsatz{arb})
-    u0_float = convert(FractionalKdVAnsatz{Float64}, u0)
-
-    findbs!(u0_float)
-    # TODO: Possibly perform some Newton iterations if the values are
-    # required to a higher precision.
-
-    u0.b .= parent(u0.α).(u0_float.b)
-
-    return u0
-end
-
 function findbs!(u0::FractionalKdVAnsatz{Arb})
     u0_float = convert(FractionalKdVAnsatz{Float64}, u0)
 
     findbs!(u0_float)
-    # TODO: Possibly perform some Newton iterations if the values are
-    # required to a higher precision.
 
     u0.b .= convert.(Arb, u0_float.b)
 
