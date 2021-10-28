9a8d1895ca6c53655477ce213a4a6c8634e00d11
Joel Dahne <joel@dahne.eu>
2021-10-28 13:52:57 -0400

    Update generic delta0 to remove Nemo support

--- src/alpha.jl
+++ src/alpha.jl
@@ -1,24 +1,22 @@
 export alpha0, alpha0_estimate
 
 """
-    alpha0(u0::AbstractAnsatz; M::Integer = 3, rtol::arb = 1e-5)
+    alpha0(u0::AbstractAnsatz{Arb})
+
 Upper bound the value of `α₀` from the paper.
 
-Specialized for different types of ansatz.
+Specialized for different types of `AbstractAnsatz`.
 """
 alpha0
 
 """
     alpha0_estimate(u0::AbstractAnsatz)
+
 Estimate the value of `α₀` from the paper. Uses the observation that
 the maximum is obtained at `x = π`. This always gives a lower bound of
 `α₀`.
+
+**TODO:** This is not necessarily true in all cases for all choices of
+weights. But since it's only an estimate it doesn't really matter.
 """
-function alpha0_estimate(u0::AbstractAnsatz{T}) where {T}
-    if T == arb
-        π = u0.parent(pi)
-    else
-        π = convert(T, pi)
-    end
-    return u0.w(π) / (2u0(π))
-end
+alpha0_estimate(u0::AbstractAnsatz{T}) where {T} = u0.w(convert(T, π)) / (2u0(covert(T, π)))
