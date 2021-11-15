6615e67622bb93c40261a96ade5c1944220a3ac6
Joel Dahne <joel@dahne.eu>
2021-11-15 14:26:27 -0500

    Update delta0 for BHKdV
    Implement an approximation of the non-asymptotic defect. We need to
    improve the enclosures when evaluating F0 before being able to
    properly enclose it.

--- src/BHKdV/delta.jl
+++ src/BHKdV/delta.jl
@@ -11,14 +11,15 @@ The interval `[0, π]` is first split into three intervals, `[0, ϵ1]`,
    with [`ArbExtras.maximum_enclosure`](@ref).
 3. On the third interval we uses straight forward ball enclosures
    together with [`ArbExtras.maximum_enclosure`](@ref).
+   - **TODO:** Implement this. For now we use an approximation
 
 **IMPROVE:** We could make the choice of the tolerances used dynamic
 and not hard coded like now.
 """
-function delta0(u0::BHKdVAnsatz{Arb}; rtol = Arb(1e-1), degree = 4, verbose = false)
+function delta0(u0::BHKdVAnsatz{Arb}; rtol = Arb("1e-1"), verbose = false)
     ϵ1 = midpoint(Arb("1e-10000"))
     ϵ2 = midpoint(Arb("1e-5"))
-    @show ϵ1 ϵ2
+
     f = F0(u0, Asymptotic())
 
     # Bound the value on [0, ϵ1] with one evaluation
@@ -49,19 +50,34 @@ function delta0(u0::BHKdVAnsatz{Arb}; rtol = Arb(1e-1), degree = 4, verbose = fa
 
     verbose && @show m2
 
-    return max(m1, m2)
-
     # Bound the value on [ϵ2, π]
-    m3 = ArbExtras.maximum_enclosure(
-        F0(u0),
-        ϵ2,
-        Arblib.ubound(Arb(π)),
-        abs_value = true,
-        threaded = true;
-        degree = -1,
-        rtol,
-        verbose,
-    )
+    g = F0(u0)
+
+    # Compute an approximation of the maximum. Take log-spaced points
+    # on [ϵ2, 1e-1] and linearly space on [1e-1, π].
+    xs1 = exp.(range(log(Arb(ϵ2)), log(Arb("1e-1")), length = 20))
+    xs2 = range(Arb("1e-1"), Arb(π), length = 20)
+    xs = [xs1; xs2]
+    ys = similar(xs)
+    Threads.@threads for i in eachindex(xs)
+        ys[i] = g(xs[i])
+    end
+
+    m3_approx = maximum(abs.(ys))
+
+    # FIXME: Implement rigorous enclosure of maximum
+    @warn "non-rigorous approximation of non-asymptotic defect"
+    m3 = m3_approx
+    #m3 = ArbExtras.maximum_enclosure(
+    #    F0(u0),
+    #    ϵ2,
+    #    Arblib.ubound(Arb(π)),
+    #    degree = -1,
+    #    abs_value = true,
+    #    threaded = true;
+    #    rtol,
+    #    verbose,
+    #)
 
     verbose && @show m3
 
