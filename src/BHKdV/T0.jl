c0b825190e5c19815767761fb472bfdb6512a61e
Joel Dahne <joel@dahne.eu>
2021-11-16 11:58:21 -0500

    Add new alternative method for computing norm for BHKdV
    It is based on T013plusT021 introduced in an earlier commit but has
    been slightly reworked.

--- src/BHKdV/T0.jl
+++ src/BHKdV/T0.jl
@@ -36,32 +36,89 @@ function T0(u0::BHKdVAnsatz, evaltype::Asymptotic; non_asymptotic_u0 = false, ϵ
     return x -> f(x) + g(x)
 end
 
+function T0_alternative(
+    u0::BHKdVAnsatz,
+    evaltype::Ball = Ball();
+    δ0::Arb = Arb(1e-5),
+    δ::Arb = Arb(1e-3),
+    skip_div_u0 = false,
+)
+    # Integration on [0, δ0]
+    f1 = T011(u0, evaltype, skip_div_u0 = true; δ0)
+    # Integration on [δ0, 1 - δ]
+    f2 = T012(u0, evaltype, skip_div_u0 = true, δ1 = δ; δ0)
+    # Integration on [1 - δ, b / x] with b picked later
+    f3 = T0_primitive(u0, evaltype, skip_div_u0 = true)
+    # Integration on [b, π] with b picked later
+    f4 = T022(u0, evaltype, skip_div_u0 = true)
+    # Construction depends on x
+
+    return x -> begin
+        part1 = f1(x)
+
+        isfinite(part1) || return part1
+
+        part2 = f2(x)
+
+        isfinite(part2) || return part2
+
+        # We want to take the upper integration independent of x
+        # in the variables used by T022. We take it to be ubound(x + δ)
+        b_y = ubound(Arb, x + δ) # Upper bound in the variable y
+        if b_y < π
+            # Compute upper bound in the variable t = y / x
+            b_t = b_y / x
+            part3 = f3(x, 1 - δ, b_t)
+
+            isfinite(part3) || return part3
+
+            part4 = f4(x, b_y)
+        else
+            # The value 1 + δ is not important
+            part3 = f3(x, 1 - δ, 1 + δ, to_endpoint = true)
+
+            part4 = zero(x)
+        end
+
+        res = part1 + part2 + part3 + part4
+
+        if skip_div_u0
+            return res
+        else
+            return res / u0(x)
+        end
+    end
+end
+
 """
-    T013plusT021(u0::BHKdVAnsatz{Arb}, ::Ball; δ::Arb)
+    T0_primitive(u0::BHKdVAnsatz{Arb}, ::Ball)
 
-Compute the part of the integral around the singularity at `y = x` by
-factoring out part of the weight and integrating explicitly.
-
-With the change of coordinates `t = y / x` we get that the integral
-for the norm is given by
+With the change of coordinates `t = y / x` the integral for the norm
+is given by
 ```
 x / (π * u0(x) * log(u0.c + inv(x))) *
     ∫ abs(clausenc(x * (t - 1), -α) + clausenc(x * (1 + t), -α) - 2clausenc(x * t, -α)) * t * log(u0.c + inv(x * t)) dt
 ```
-We are interested in computing this integral around the point `t = 1`
-where it has a singularity. More precisely we compute it on the
-interval `[1 - δ, 1 + δ]`.
+This method returns a function `f` such that `f(x, a, b)` computes the
+above, integrating `a` to `b`. Optionally it accepts the keyword
+argument `to_endpoint` which if true makes it compute the integral
+from `a` to `π / x`, i.e. `f(x, a, b, to_endpoint = true)`.
 
-Since `log(u0.c + inv(x * t))` is well behaved as long as `x` is not
-very close to zero we can compute an enclosure of it for all `t ∈ [1 -
-δ, 1 + δ]` and factor this out. This leaves us with the integral
+The computation is done by factoring out part of the weight and
+integrating the rest explicitly.
+
+More precisely, since `log(u0.c + inv(x * t))` is well behaved as long
+as `x` is not very close to zero we can compute an enclosure of it for
+all `t ∈ [1 - δ, 1 + δ]` and factor this out. This leaves us with the
+integral
 ```
 I = ∫ (clausenc(x * (t - 1), -α) + clausenc(x * (1 + t), -α) - 2clausenc(x * t, -α)) * t dt
 ```
-taken from `1 - δ` to `1 + δ`. Where we have removed the absolute
+taken from `a` to `b` (or `π / x`). Where we have removed the absolute
 value since the expression inside is positive on the interval.
-- **PROVE:** That the expression inside is positive, this holds as
-  long as `δ` is not too large.
+- **TODO:** We need to check the positivity of the integrand. This
+  could be done by checking the endpoint `a`. However for it to work
+  well for wide values of `x` we might need to do some more work.
 
 We can integrate this explicitly, see [`T0_p_one`](@ref), giving us
 ```
@@ -75,54 +132,36 @@ Call this function `primitive(t)`. We then have
 I = primitive(1 + δ) - primitive(1 - δ)
 ```
 
-In our case `α` is the interval `[-1, -1 + u0.ϵ]` and we let `s1 = 1 -
-α` and `s2 = 2 - α`.
+By reorganising the terms we can rewrite `primitive(t)` as
+```
+primitive(t) =
+    (clausenc(x * (1 - t), 2 - α) + clausenc(x * (1 + t), 2 - α) - 2clausenc(x * t, 2 - α)) / x^2 -
+    t * (clausens(x * (1 - t), 1 - α) - clausens(x * (1 + t), 1 - α) + 2clausens(x * t, 1 - α)) / x
+```
+We can also improve the computed enclosures by replacing `clausenc`
+with `clausencmzeta`. The constants always cancel out and
+`clausencmzeta` has better monotonicity properties in the parameter,
+giving tighter enclosures. Giving us
+```
+primitive(t) =
+    (clausencmzeta(x * (1 - t), 2 - α) + clausencmzeta(x * (1 + t), 2 - α) - 2clausencmzeta(x * t, 2 - α)) / x^2 -
+    t * (clausens(x * (1 - t), 1 - α) - clausens(x * (1 + t), 1 - α) + 2clausens(x * t, 1 - α)) / x
+```
 
 We can notice that `primitive(t)` contains a division by `x` for all
 terms and that we in the end want to multiply the whole integral by
 `x`. To cancel these two we therefore let
 ```
-primitive_mul_x(t) = (clausenc(x * (1 - t), 2 - α) / x - t * clausens(x * (1 - t), 1 - α)) +
-    (clausenc(x * (1 + t), 2 - α) / x + t * clausens(x * (1 + t), 1 - α)) -
-    2(clausenc(x * t, 2 - α) / x + t * clausens(x * t, 1 - α))
-```
-which we can rewrite as
-```
-primitive_mul_x(t) = (
-        clausenc(x * (1 - t), 2 - α) + clausenc(x * (1 + t), 2 - α) - 2clausenc(x * t, 2 - α)
-    ) / x + t * (
-        -clausens(x * (1 - t), 1 - α) + clausens(x * (1 + t), 1 - α) + 2clausens(x * t, 1 - α)
-    )
+primitive_mul_x(t) =
+    (clausencmeta(x * (1 - t), 2 - α) + clausencmzeta(x * (1 + t), 2 - α) - 2clausencmzeta(x * t, 2 - α)) / x -
+    t * (clausens(x * (1 - t), 1 - α) - clausens(x * (1 + t), 1 - α) + 2clausens(x * t, 1 - α))
 ```
 
-Now specialising for the case `t = 1 - δ` and `t = 1 + δ` we get
+# Value at `t = π / x`
+In the special case that `t = π / x` this simplifies to
 ```
-(
-    clausenc(x * δ, 2 - α) + clausenc(x * (2 - δ), 2 - α) - 2clausenc(x * (1 - δ), 2 - α)
-) / x + (1 - δ) * (
-    -clausens(x * δ, 1 - α) + clausens(x * (2 - δ), 1 - α) + 2clausens(x * (1 - δ), 1 - α)
-)
-```
-and
-```
-(
-    clausenc(x * δ, 2 - α) + clausenc(x * (2 + δ), 2 - α) - 2clausenc(x * (1 + δ), 2 - α)
-) / x + (1 + δ) * (
-    clausens(x * δ, 1 - α) + clausens(x * (2 + δ), 1 - α) + 2clausens(x * (1 + δ), 1 - α)
-)
-```
-where we have used that `clausenc` is even and `clausens` is odd.
-
-**IMPROVE:** We can use `ArbSeries` to expand in `x` and compute a
-tighter enclosure for most of it.
-
-# Handling `x` close to `π`
-If `x` is sufficiently close to `π` then `1 + δ > π / x` and our
-approach needs to be modified. In that case we are only want to
-integrate up to `π / x` and we are hence interested in computing
-`primitive_mul_x(π / x)`, for which we have
-```
-primitive_mul_x(π / x) = 2(clausenc(x + π, 2 - α) - clausenc(Arb(π), 2 - α)) / x
+primitive_mul_x(π / x) = 2(clausencmzeta(x + π, 2 - α) - clausencmzeta(Arb(π), 2 - α)) / x
+    = 2(clausenc(x + π, 2 - α) - clausenc(Arb(π), 2 - α)) / x
 ```
 where `clausenc(Arb(π), 2 - α)` can also be given as the negated
 alternating zeta function, `-eta(2 - α)`. Evaluating `clausenc(x + π,
@@ -133,120 +172,123 @@ for `x` close to `π` we want to use the expansion at `x = 0`. However
 in our case `2 - α` overlaps with `3` and hence the expansion is
 singular. We therefore use
 [`clausenc_expansion_odd_s_singular`](@ref).
+
+# Handling wide values of `a` and `b`
+If either `a` or `b` is a wide ball we can compute a tighter enclosure
+by using that `primitive_mul_x(t)` is increasing in `t`. That it is
+increasing is an easy consequence of being the primitive function of a
+positive integrand.
+
+**IMPROVE:** We can use `ArbSeries` to expand in `x` and compute a
+tighter enclosure for most of it.
 """
-function T013plusT021(
-    u0::BHKdVAnsatz{Arb},
-    evaltype::Ball = Ball();
-    δ::Arb = Arb(1e-3),
-    skip_div_u0 = false,
-)
-    ϵ = u0.ϵ
+function T0_primitive(u0::BHKdVAnsatz{Arb}, evaltype::Ball = Ball(); skip_div_u0 = false)
+    #Enclosure of Arb((2 - u0.ϵ, 3)) computed in a way so that the
+    #upper endpoint is exactly 2
+    s1 = 2 - Arblib.nonnegative_part!(zero(Arb), Arb((0, u0.ϵ)))
+    #Enclosure of Arb((3 - u0.ϵ, 3)) computed in a way so that the
+    #upper endpoint is exactly 3
+    s2 = 3 - Arblib.nonnegative_part!(zero(Arb), Arb((0, u0.ϵ)))
 
-    # s1 = 1 + α
-    s1 = Arb((2 - ϵ, 2))
-    # s2 = 2 - α
-    s2 = Arb((3 - ϵ, 3))
-
-    return x -> begin
+    return (x::Arb, a::Arb, b::Arb; to_endpoint = false) -> begin
         primitive(t) =
-            (clausenc(x * (1 - t), s2) / x^2 - t * clausens(x * (1 - t), s1) / x) +
-            (clausenc(x * (1 + t), s2) / x^2 + t * clausens(x * (1 + t), s1) / x) -
-            2(clausenc(x * t, s2) / x^2 + t * clausens(x * t, s1) / x)
+            (
+                clausencmzeta(x * (1 - t), s2) + clausencmzeta(x * (1 + t), s2) -
+                2clausencmzeta(x * t, s2)
+            ) / x^2 -
+            t * (
+                clausens(x * (1 - t), s1) - clausens(x * (1 + t), s1) +
+                2clausens(x * t, s1)
+            ) / x
 
         primitive_mul_x(t) =
-            (clausenc(x * (1 - t), s2) / x - t * clausens(x * (1 - t), s1)) +
-            (clausenc(x * (1 + t), s2) / x + t * clausens(x * (1 + t), s1)) -
-            2(clausenc(x * t, s2) / x + t * clausens(x * t, s1))
-
-        primitive_1mδ_mul_x =
-            (clausenc(x * δ, s2) + clausenc(x * (2 - δ), s2) - 2clausenc(x * (1 - δ), s2)) / x +
-            (1 - δ) * (
-                -clausens(x * δ, s1) + clausens(x * (2 - δ), s1) -
-                2clausens(x * (1 - δ), s1)
+            (
+                clausencmzeta(x * (1 - t), s2) + clausencmzeta(x * (1 + t), s2) -
+                2clausencmzeta(x * t, s2)
+            ) / x -
+            t * (
+                clausens(x * (1 - t), s1) - clausens(x * (1 + t), s1) +
+                2clausens(x * t, s1)
             )
 
-        primitive_1pδ_mul_x =
-            (clausenc(x * δ, s2) + clausenc(x * (2 + δ), s2) - 2clausenc(x * (1 + δ), s2)) / x +
-            (1 + δ) * (
-                clausens(x * δ, s1) + clausens(x * (2 + δ), s1) -
-                2clausens(x * (1 + δ), s1)
-            )
+        # Compute primitive_mul_x(a) using the monotonicity in a
+        primitive_a_mul_x = if iswide(a)
+            Arb((primitive_mul_x(lbound(Arb, a)), primitive_mul_x(ubound(Arb, a))))
+        else
+            primitive_mul_x(a)
+        end
 
-        # IMPROVE: We don't need to compute this in general
-        primitive_pidivx_mul_x = let
-            # Compute an enclosure of clausenc(π - x, s2)
-            if Arblib.overlaps(x, Arb(π))
-                # Enclosure of π - x
-                pimx = Arblib.nonnegative_part!(zero(x), π - x)
+        if b < π && !to_endpoint
+            # We integrate on [a, b]
+            weight_enclosure = log(u0.c + inv(x * Arb((a, b))))
 
-                # Compute the analytic terms in the expansion
-                C, _, p, E = clausenc_expansion(ubound(Arb, pimx), s2, 3)
-
-                @assert !isfinite(C)
-
-                # Set the singular term to zero, we handle it separately.
-                @assert !isfinite(p[2])
-                p[2] = 0
-
-                # Factor out x^1.5
-                exponent = Arb(3 // 2)
-                D = clausenc_expansion_odd_s_singular(ubound(Arb, pimx), s2, exponent)
-                clausenc_pimx = D * pimx^exponent + p(pimx)
+            # Compute primitive_mul_x(b) using the monotonicity in b
+            primitive_b_mul_x = if iswide(b)
+                Arb((primitive_mul_x(lbound(Arb, b)), primitive_mul_x(ubound(Arb, b))))
             else
-                clausenc_pimx = clausenc(π - x, s2)
+                primitive_mul_x(b)
             end
 
-            2(clausenc_pimx + real(eta(Acb(s2)))) / x
-        end
+            I_mul_x = primitive_b_mul_x - primitive_a_mul_x
+        elseif to_endpoint || b * x > π || contains(π / x, b)
+            # We integrate on [a, π / x]
+            weight_enclosure = log(u0.c + inv(x * Arb((a, π / x))))
 
-        @assert Arblib.overlaps(primitive(1 - δ) * x, primitive_1mδ_mul_x)
-        @assert Arblib.overlaps(primitive(1 + δ) * x, primitive_1pδ_mul_x)
-        @assert Arblib.overlaps(primitive(π / x) * x, primitive_pidivx_mul_x)
+            # Enclosure of primitive_mul_x(π / x)
+            primitive_pidivx_mul_x = let
+                # Enclosure of π - x using that x <= π
+                pimx = Arblib.nonnegative_part!(zero(x), π - x)
 
-        if 1 + δ < π / x
-            # Standard case, when the upper integration limit is 1 + δ
-            I_mul_x = primitive_1pδ_mul_x - primitive_1mδ_mul_x
-        elseif 1 + δ > π / x
-            # In this case we only want to integrate up to π / x
-            # instead of 1 + δ
-            I_mul_x = primitive_pidivx_mul_x - primitive_1mδ_mul_x
-        elseif contains(π / x, 1 + δ)
-            # In this case the integration limit depends on the
-            # precise choice of x. However we can get an enclosure by
-            # just using π / x
-            I_mul_x = primitive_pidivx_mul_x - primitive_1mδ_mul_x
+                # Compute an enclosure of clausenc(π - x, s2)
+                if Arblib.overlaps(x, Arb(π))
+                    # Compute the analytic terms in the expansion
+                    C, _, p, E = clausenc_expansion(ubound(Arb, pimx), s2, 3)
+
+                    @assert !isfinite(C)
+
+                    # Set the singular term to zero, we handle it separately.
+                    @assert !isfinite(p[2])
+                    p[2] = 0
+
+                    # Factor out x^1.5
+                    exponent = Arb(3 // 2)
+                    D = clausenc_expansion_odd_s_singular(ubound(Arb, pimx), s2, exponent)
+                    clausenc_pimx = D * pimx^exponent + p(pimx)
+                else
+                    clausenc_pimx = clausenc(pimx, s2)
+                end
+
+                2(clausenc_pimx + real(eta(Acb(s2)))) / x
+            end
+
+            I_mul_x = primitive_pidivx_mul_x - primitive_mul_x(a)
         else
-            # In this case the integration limit also depends on the
-            # precise choice of x. We could take the union of
-            # primitive_1pδ_mul_x and primitive_pidivx_mul_x but the
-            # first one of these will always be non-finite, so we just
-            # return an indeterminate result directly.
-            # IMPROVE: We could compute an enclosure in this case as
-            # well, however I'm not sure it ever happens?
-            @warn "π / x and 1 + δ overlaps" π / x 1 + δ
+            # In this case it is unclear what integration limit we
+            # should use. Make a warning and return NaN. This should
+            # not happen in practice.
+            @warn "Unclear integration limit" b π / x
             return Arblib.indeterminate!(zero(x))
         end
-
-        # Compute enclosure of log(u0.c + inv(x * t))
-        t_interval = Arblib.add_error!(one(x), δ)
-        weight_enclosure = log(u0.c + inv(x * t_interval))
 
         # Enclosure of factor in front of integral divided by x, excluding u0
         factor_div_x = inv(π * log(u0.c + inv(x)))
 
         res = factor_div_x * weight_enclosure * I_mul_x
 
-        # Other version for computing it
-        # T013 uses the variable t as well so δ1 = δ
-        f = T013(u0, evaltype, δ1 = δ, skip_div_u0 = true)
-        # T021 uses the variable y directly, so 1 + δ corresponds to x
-        # + x * δ, hence δ2 = x * δ.
-        g = T021(u0, evaltype, δ2 = x * δ, skip_div_u0 = true)
-        part1 = abs(f(x))
-        part2 = abs(g(x))
-        res2 = part1 + part2
+        # Other version for computing it, can be used for checking.
+        # Though it doesn't support all cases.
+        if false
+            # T013 uses the variable t as well so we get δ1 = 1 - a
+            f = T013(u0, evaltype, δ1 = 1 - a, skip_div_u0 = true)
+            # T021 uses the variable y directly, so b corresponds to x *
+            # b, hence δ2 = x * (b - 1).
+            g = T021(u0, evaltype, δ2 = x * (b - 1), skip_div_u0 = true)
+            part1 = abs(f(x))
+            part2 = abs(g(x))
+            res2 = part1 + part2
 
-        @assert Arblib.overlaps(res, res2)
+            @assert Arblib.overlaps(res, res2)
+        end
 
         if skip_div_u0
             return res
