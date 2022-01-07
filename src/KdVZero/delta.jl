aa150462bece308ae8bc9e2f8948c0dda4ef6996
Joel Dahne <joel@dahne.eu>
2022-01-07 08:48:02 -0500

    KdVZero: Update delta0

--- src/KdVZero/delta.jl
+++ src/KdVZero/delta.jl
@@ -12,22 +12,14 @@ satisfying that `p(α)` gives an enclosure of \$δ_0\$ for every `α ∈
 u0.α`.
 
 For a given value of `x` [`F0`](@ref) gives us an expansion in `α`.
-From this expansion we compute `p₂(x)` such that
+We truncate this expansion to degree `2` using
+[`trunace_with_remainder`](@ref), giving us a polynomial of the form
 ```
 0 + 0 * α + p₂(x) * α^2
 ```
-gives an enclosure of `abs(F0(u0)(x))` for every `α ∈ u0.α`. We are then
-interested in computing the maximum value of `p₂(x)` for `x ∈ [0, π]`.
-
-To compute `p₂(x)` we note that `F0(u0)(x)` returns `q::ArbSeries`
-such that `q(u0.α)` gives an enclosure for the specified `x`. We can
-reduce this to a second order enclosure by rewriting `q` as
-```
-q = 0 + 0 * α + q[2] * α^2 + q[3] * α^3 + ...
-  = (q[2] + q[3] * α + ...) * α^2
-```
-and we see that it is enough to compute an enclosure of `q[2] + q[3] *
-α + ...`.
+that gives an enclosure of `abs(F0(u0)(x))` for every `α ∈ u0.α`. We
+are then interested in computing the maximum value of `abs(p₂(x))` for
+`x ∈ [0, π]`.
 
 The interval `[0, π]` is split into two parts, `[0, ϵ]` and ´[ϵ, π]`.
 On `[0 ϵ]` we use the asymptotic version of `F0(u0)` whereas on ´[ϵ,
@@ -35,24 +27,51 @@ On `[0 ϵ]` we use the asymptotic version of `F0(u0)` whereas on ´[ϵ,
 """
 function delta0(
     u0::KdVZeroAnsatz;
-    ϵ::Arf = Arf(0.1),
-    rtol = Arb(1e-3),
-    maxevals = 1000, # TODO: This we can probably remove later
+    rtol = Arb(1e-2),
+    atol = Arb(1e-3),
     threaded = true,
     verbose = false,
+    maxevals = 10000, # A good value for this depends on u0.α
 )
-    # In both the below methods we compute the expansion, divide by
-    # α^2 and evaluate the resulting series at u0.α. This gives us an
-    # enclosure of p₂(x). Note that the division by α checks so that
-    # the first two coefficients are exactly zero.
+    # Determine a good choice of ϵ. Take it as large as possible so
+    # that the asymptotic version still satisfies the required
+    # tolerance when evaluated at ϵ
+    ϵ = let ϵ = Arb(π), f = F0(u0, Asymptotic(), ϵ = ubound(Arb, ϵ))
+        y = f(ϵ)[2]
+
+        # Reduce ϵ until the value we get satisfies the required
+        # tolerance
+        while !ArbExtras.check_tolerance(y; rtol, atol)
+            if ϵ > 1
+                ϵ -= 0.05
+            else
+                ϵ /= 1.2
+            end
+            y = f(ϵ)[2]
+            ϵ > 0.1 || error("could not determine working ϵ, last tried value was $ϵ")
+        end
+        ubound(ϵ)
+    end
+
+    verbose && @info "Determined ϵ" ϵ
 
     # Function for computing p₂(x) for x ∈ [0, ϵ]
-    F0_asymptotic = F0(u0, Asymptotic(), ϵ = Arb(2ϵ))
-    f(x) = (F0_asymptotic(x) << 2)(u0.α)
+    F0_asymptotic = F0(u0, Asymptotic(), ϵ = Arb(1.1ϵ))
+    f(x) =
+        let
+            res = F0_asymptotic(x)
+            @assert Arblib.valuation(res) == 2 # Check that p[0] and p[1] are zero
+            truncate_with_remainder(res, u0.α, degree = 2)[2]
+        end
 
     # Function for computing p₂(x) for x ∈ [ϵ, π]
     F0_nonasymptotic = F0(u0, Ball())
-    g(x) = (F0_nonasymptotic(x) << 2)(u0.α)
+    g(x) =
+        let
+            res = F0_nonasymptotic(x)
+            @assert Arblib.valuation(res) == 2 # Check that p[0] and p[1] are zero
+            truncate_with_remainder(res, u0.α, degree = 2)[2]
+        end
 
     # Compute an enclosure on [0, ϵ]
     p2_asymptotic = ArbExtras.maximum_enclosure(
@@ -62,6 +81,7 @@ function delta0(
         degree = -1,
         abs_value = true;
         rtol,
+        atol,
         threaded,
         verbose,
     )
@@ -76,6 +96,7 @@ function delta0(
         degree = -1,
         abs_value = true;
         rtol,
+        atol,
         maxevals,
         threaded,
         verbose,
