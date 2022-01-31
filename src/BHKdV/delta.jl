285cd0812e871edf4fb5265b786d357f014d4fb7
Joel Dahne <joel@dahne.eu>
2022-01-31 11:04:31 -0500

    BHKdV: Update delta0
    This takes into account all the changes that have been made to F0
    recently. It also makes it pick ϵ in a dynamic way. All of this
    greatly increases the part of the interval that can be handled using
    the asymptotic expansion, giving much better performance.

--- src/BHKdV/delta.jl
+++ src/BHKdV/delta.jl
@@ -1,58 +1,104 @@
 """
-    delta0(u0::BHKdVAnsatz{Arb})
+    delta0(u0::BHKdVAnsatz{Arb}; atol = Arb(0), rtol = Arb(1e-1), verbose = false)
 
 Enclose the value of `δ₀` from the paper.
 
-The interval `[0, π]` is first split into three intervals, `[0, ϵ1]`,
-`[ϵ1, ϵ2]` and `[ϵ2, π]`.
+The interval `[0, π]` is split into three intervals, `[0, ϵ1]`, `[ϵ1,
+ϵ]` and `[ϵ, π]`.
 1. On the first interval we evaluate `F0` directly using the
    asymptotic expansion.
 2. On the second interval we use the asymptotic expansion together
    with [`ArbExtras.maximum_enclosure`](@ref).
-3. On the third interval we use [`F0_upper_bound`](@ref) together with
+3. On the third interval we use [`F0_bound`](@ref) together with
    [`ArbExtras.maximum_enclosure`](@ref).
 
-**IMPROVE:** We could make the choice of the tolerances used dynamic
-and not hard coded like now.
+The value for `ϵ` is taken as large as possible but so that the
+asymptotic evaluation still satisfies the required tolerance. We
+enforce that `ϵ > 1e-2`.
+
+In practice the maximum is attained around `1e-2` so it computes an
+approximation of the maximum on `[1e-10, ϵ]`. It then looks for `ϵ1`
+such that the enclosure for the interval `[0, ϵ1]` is less than this
+approximate maximum. The interval `[ϵ1, ϵ]` is then handled by
+bisection.
 """
-function delta0(u0::BHKdVAnsatz{Arb}; rtol = Arb("1e-1"), verbose = false)
-    ϵ1 = midpoint(Arb("1e-10000"))
-    ϵ2 = midpoint(Arb("1e-5"))
+function delta0(u0::BHKdVAnsatz{Arb}; atol = Arb(0), rtol = Arb(1e-1), verbose = false)
+    # Determine a good choice of ϵ. Take it as large as possible so
+    # that the asymptotic version still satisfies the specified
+    # tolerance or it is better than the non-asymptotic version.
+    ϵ = let ϵ = Arb(0.8), f = F0(u0, Asymptotic(), ϵ = 1.01ϵ, M = 10), g = F0(u0)
+        y = f(ϵ)
+        z = g(ϵ)
 
-    f = F0(u0, Asymptotic())
+        # Reduce ϵ until the value we get either satisfies the
+        # required tolerance or is better than the non-asymptotic
+        # version.
+        while !ArbExtras.check_tolerance(y; atol, rtol) && radius(y) > radius(z)
+            ϵ /= 1.2
+            y = f(ϵ)
+            z = g(ϵ)
+            ϵ > 1e-2 || error("could not determine working ϵ, last tried value was $ϵ")
+        end
+        midpoint(ϵ)
+    end
 
-    # Bound the value on [0, ϵ1] with one evaluation
-    m1 = f(Arb((0, ϵ1)))
+    verbose && @info "Determined ϵ" ϵ
 
-    verbose && @show m1
+    # We use two versions of the function for asymptotic evaluation,
+    # with M = 3 and M = 10. We use the former for x < δ and the
+    # latter otherwise
+    δ = Arb(1e-2)
+    f1 = F0(u0, Asymptotic(), ϵ = 1.01Arb(ϵ), M = 3)
+    f2 = F0(u0, Asymptotic(), ϵ = 1.01Arb(ϵ), M = 10)
+    f = x -> x < δ ? f1(x) : f2(x)
 
-    # Bound on [ϵ1, ϵ2]
+    # Compute an approximation of the maximum on [1e-10, ϵ]
+    xs_asym = exp.(range(log(Arb(1e-10)), log(Arb(ϵ)), length = 24))
+    ys_asym = similar(xs_asym)
+    Threads.@threads for i in eachindex(xs_asym)
+        ys_asym[i] = f(xs_asym[i])
+    end
+    m_asym_approx = maximum(abs.(ys_asym))
+
+    verbose && @info "Approximate maximum on [1e-10, ϵ]" m_asym_approx
+
+    # Find ϵ1 such that the interval [0, ϵ1] can be handled in one
+    # evaluation
+    N = -100 # Use the interval [0, 10^-N]
+    m1 = f(Arb((0, Arb(10)^N)))
+    while !(m1 < m_asym_approx)
+        N -= 100
+        m1 = f(Arb((0, Arb(10)^N)))
+        N > -20000 || error("could not determine working N, last tried value was $N")
+    end
+    ϵ1 = lbound(Arb(10)^N)
+
+    verbose && @info "Determined ϵ1" ϵ1
+
+    # Bound on [0, ϵ]
     m2 = ArbExtras.maximum_enclosure(
-        F0(u0, Asymptotic(), ϵ = 2Arb(ϵ2)),
+        f,
         ϵ1,
-        ϵ2,
+        ϵ,
         abs_value = true,
         log_bisection = true,
-        point_value_max = f(Arb(ϵ2)),
+        point_value_max = m_asym_approx,
         threaded = true,
         degree = -1,
-        depth = 30,
-        maxevals = 20000,
-        atol = Arb("2e-4");
+        depth = 35,
+        maxevals = 200000;
+        atol,
         rtol,
         verbose,
     )
 
-    verbose && @show m2
+    verbose && @info "Maximum on [ϵ1, ϵ]" m2
 
     # Bound the value on [ϵ2, π]
-    g = F0_upper_bound(u0)
+    g = F0_bound(u0)
 
-    # Compute an approximation of the maximum. Take log-spaced points
-    # on [ϵ2, 1e-1] and linearly space on [1e-1, π].
-    xs1 = exp.(range(log(Arb(ϵ2)), log(Arb("1e-1")), length = 20))
-    xs2 = range(Arb("1e-1"), Arb(π), length = 20)
-    xs = [xs1; xs2]
+    # Compute an approximation of the maximum on [ϵ, π]
+    xs = range(Arb(ϵ), π, length = 20)
     ys = similar(xs)
     Threads.@threads for i in eachindex(xs)
         ys[i] = g(xs[i])
@@ -60,11 +106,11 @@ function delta0(u0::BHKdVAnsatz{Arb}; rtol = Arb("1e-1"), verbose = false)
 
     m3_approx = maximum(abs.(ys))
 
-    verbose && @show m3_approx
+    verbose && @info "Approximate maximum on [ϵ, π]" m3_approx
 
     m3 = ArbExtras.maximum_enclosure(
         g,
-        ϵ2,
+        ϵ,
         ubound(Arb(π)),
         degree = 4,
         abs_value = true,
@@ -77,7 +123,7 @@ function delta0(u0::BHKdVAnsatz{Arb}; rtol = Arb("1e-1"), verbose = false)
         verbose,
     )
 
-    verbose && @show m3
+    verbose && @info "Maximum in [ϵ, π]" m3
 
     return max(m1, m2, m3)
 end
