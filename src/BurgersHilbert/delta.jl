19ccddefd216be58fb6e0ddd1586069544925598
Joel Dahne <joel@dahne.eu>
2022-03-11 17:38:04 +0100

    BH: Update delta0 to also return maximum on subintervals

--- src/BurgersHilbert/delta.jl
+++ src/BurgersHilbert/delta.jl
@@ -1,5 +1,5 @@
 """
-    delta0(u0::BHAnsatz{Arb})
+    delta0(u0::BHAnsatz{Arb}; rtol, degree, verbose)
 
 Enclose the value of `δ₀` from the paper.
 
@@ -28,12 +28,22 @@ to `1 // 4`. On the interval `[ϵ3, ϵ]` we keep all the exponent.
 reduced precision. However this seems to only give a marginal
 improvement. It would be more important to reduce the number of
 allocations in that case.
+
+# Arguments
+- `rtol = 1e-3`: Determines the relative tolerance used when enclosing
+  the maximum.
+- `degree = 4`: Determines the degree used when enclosing the maximum.
+- `return_subresults`: If true it returns not only the maximum on the
+  interval but the maximum on each of the intervals `[0, ϵ1]`, `[ϵ1,
+  ϵ2]`, `[ϵ2, ϵ3]`, `[ϵ3, ϵ]` and `[ϵ, π]`.
+- `verbose = false`: If true it prints more information about the
+  progress.
 """
 function delta0(
     u0::BHAnsatz{Arb};
     rtol = Arb(1e-3),
     degree = 4,
-    maxevals = 5000,
+    return_subresults = false,
     verbose = false,
 )
     ϵ = Arblib.midpoint(Arb("1e-1"))
@@ -52,9 +62,22 @@ function delta0(
     f2 = F0(u0, Asymptotic(), exponent_limit = Arb(1 // 4), ϵ = 2Arb(ϵ3))
     f3 = F0(u0, Asymptotic(), exponent_limit = nothing, ϵ = 2Arb(ϵ), M = 5)
 
+    verbose &&
+        @info "Terms in expansion on [0, ϵ2]" length(f1.u0_expansion_div_xlogx) length(
+            f1.Du0_expansion_div_x2logx,
+        )
+    verbose &&
+        @info "Terms in expansion on [ϵ2, ϵ3]" length(f2.u0_expansion_div_xlogx) length(
+            f2.Du0_expansion_div_x2logx,
+        )
+    verbose &&
+        @info "Terms in expansion on [ϵ3, ϵ]" length(f3.u0_expansion_div_xlogx) length(
+            f3.Du0_expansion_div_x2logx,
+        )
+
     m11 = f1(Arb((0, ϵ1)))
 
-    verbose && @show m11
+    verbose && @info "Enclosure on [0, ϵ1]" m11
 
     m12 = ArbExtras.maximum_enclosure(
         f1,
@@ -71,6 +94,8 @@ function delta0(
         verbose,
     )
 
+    verbose && @info "Enclosure on [ϵ1, ϵ2]" m12
+
     m13 = ArbExtras.maximum_enclosure(
         f2,
         ϵ2,
@@ -85,6 +110,8 @@ function delta0(
         rtol,
         verbose,
     )
+
+    verbose && @info "Enclosure on [ϵ2, ϵ3]" m13
 
     m14 = ArbExtras.maximum_enclosure(
         f3,
@@ -101,9 +128,11 @@ function delta0(
         verbose,
     )
 
+    verbose && @info "Enclosure on [ϵ3, ϵ]" m14
+
     m1 = max(m11, m12, m13, m14)
 
-    verbose && @show m1
+    verbose && @info "Enclosure on [0, ϵ]" m1
 
     # Bound the value on [ϵ, π]
     m2 = ArbExtras.maximum_enclosure(
@@ -112,12 +141,18 @@ function delta0(
         Arblib.ubound(Arb(π)),
         abs_value = true,
         depth_start = 7,
-        threaded = true;
+        threaded = true,
+        maxevals = 5000;
         degree,
-        maxevals,
         rtol,
         verbose,
     )
 
-    return max(m1, m2)
+    verbose && @info "Enclosure on [ϵ, π]" m2
+
+    if return_subresults
+        return max(m1, m2), m11, m12, m13, m14, m2
+    else
+        return max(m1, m2)
+    end
 end
