7b1fc3e1e7d6d99811fed941df38cf006a0c6cee
Joel Dahne <joel@dahne.eu>
2022-05-23 20:06:22 +0200

    Update KdVZero tests to set precision before running

--- test/KdVZero/proof.jl
+++ test/KdVZero/proof.jl
@@ -1,38 +1,42 @@
 @testset "proof" begin
     verbose = true
     @testset "α0 = 0" begin
-        ϵ = Arb(-1.2e-3)
-        u0 = KdVZeroAnsatz(Arb((ϵ, 0)))
+        setprecision(Arb, 100) do
+            ϵ = Arb(-1.2e-3)
+            u0 = KdVZeroAnsatz(Arb((ϵ, 0)))
 
-        n₀ = n0_bound(u0; verbose)
+            n₀ = n0_bound(u0; verbose)
 
-        @test 1.56 < n₀ < 1.58
+            @test 1.56 < n₀ < 1.58
 
-        A = delta0_bound(u0, maxevals = 10000, atol = Arb(0.0025); verbose)[2]
+            A = delta0_bound(u0, maxevals = 10000, atol = Arb(0.0025); verbose)[2]
 
-        @test -9e-3 < abs_ubound(A) < 9e-3
+            @test -9e-3 < abs_ubound(A) < 9e-3
 
-        B = D0_bound(u0; verbose)[1]
+            B = D0_bound(u0; verbose)[1]
 
-        @test 0.23 < B < 0.25
+            @test 0.23 < B < 0.25
 
-        @test A < B^2 / 4n₀
+            @test A < B^2 / 4n₀
+        end
     end
 
     @testset "α0 = -1 / 6" begin
-        interval = Arb((-1 // 6, -0.1))
-        n = 1000
-        α = HighestCuspedWave.mince(interval, n)[1]
-        u0 = KdVZeroAnsatz(α, midpoint(Arb, α))
+        setprecision(Arb, 100) do
+            interval = Arb((-1 // 6, -0.1))
+            n = 1000
+            α = HighestCuspedWave.mince(interval, n)[1]
+            u0 = KdVZeroAnsatz(α, midpoint(Arb, α))
 
-        proof_data = HighestCuspedWave.prove(u0; verbose)
+            proof_data = HighestCuspedWave.prove(u0; verbose)
 
-        @test proof_data.proved
+            @test proof_data.proved
 
-        @test 1.348 < proof_data.n₀ < 1.350
+            @test 1.348 < proof_data.n₀ < 1.350
 
-        @test 5e-5 < proof_data.δ₀ < 7e-5
+            @test 5e-5 < proof_data.δ₀ < 7e-5
 
-        @test 0.97 < proof_data.D₀_estimate < 0.99
+            @test 0.97 < proof_data.D₀_estimate < 0.99
+        end
     end
 end
