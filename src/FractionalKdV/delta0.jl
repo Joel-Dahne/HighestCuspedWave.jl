327457465ae41eceef64144eb29ae8fd15dcc0dd
Joel Dahne <joel@dahne.eu>
2022-11-08 09:56:04 +0100

    KdV: Update delta0_bound
    The main change is to make it compatible with use_bhkdv. It changes
    the way the interval to use the asymptotic expansion on is computed
    and tunes some parameters.

--- src/FractionalKdV/delta0.jl
+++ src/FractionalKdV/delta0.jl
@@ -1,31 +1,41 @@
 """
     delta0_bound(u0::FractionalKdVAnsatz; ϵ, M, degree, rtol, threaded, verbose)
 
-Enclose the value of `δ₀` from the paper.
+Enclose the value of `δ₀` from the paper. This is the supremum of
+`F0(u0)` for `0 < x < π`.
 
-Uses an asymptotic expansion with `M` terms on the interval `[0, ϵ]`
-and ball arithmetic together with Taylor expansions of degree `degree`
-on `[ϵ, π]`.
+Uses an asymptotic expansion with `M` terms close to zero and ball
+arithmetic on the remaining.
+
+The interval is split into three subintervals ``[0, ϵ1]``, ``[ϵ1,
+ϵ2]`` and ``[ϵ2, π]``. On the first two the asymptotic expansion is
+used and on the third ball arithmetic.
+
+The values for `ϵ1` and `ϵ2` are determined automatically. The value
+of `ϵ2` is taken such that evaluation with the asymptotic expansion
+either satisfies the required tolerance or gives a smaller error than
+evaluation with ball arithmetic. The value of `ϵ1` is taken such that
+the interval ``[0, ϵ1]`` can be handled in one evaluation.
 
 # Arguments
-- `ϵ::Arf = zero(Arf)`: Determines the interval ``[0, ϵ]`` on which
-  the asymptotic version is used. If `ϵ` is zero it automatically
-  finds a suitable value for it.
-- `M::Integer = 3`: Number of terms to use in the asymptotic
+- `M::Integer = 5`: Number of terms to use in the asymptotic
   expansions.
-- `degree::Integer = 6`: Degree used for
+- `degree::Integer = ifelse(u0.N0 > 100, 4, 6)`: Degree used for
   [`ArbExtras.maximum_enclosure`](@ref).
 - `rtol = Arb(1e-3)`: Relative tolerance used when computing maximum.
+- `ubound_tol = Arb(-Inf)`: Any number less than this is determined to
+  satisfy the tolerance. Useful if you only need to determine if the
+  maximum is less than some given number.
 - `threaded = true`: If true it enables threading when calling
   [`ArbExtras.maximum_enclosure`](@ref).
 - `verbose = false`: Print information about the process.
 """
 function delta0_bound(
     u0::FractionalKdVAnsatz{Arb};
-    ϵ::Arf = zero(Arf),
-    M::Integer = 3,
-    degree::Integer = 6,
+    M::Integer = 5,
+    degree::Integer = ifelse(u0.N0 > 100, 4, 6),
     rtol = Arb(1e-3),
+    ubound_tol = Arb(-Inf),
     threaded = true,
     verbose = false,
 )
@@ -34,70 +44,91 @@ function delta0_bound(
     # For non-asymptotic evaluation
     g = F0(u0)
 
-    if iszero(ϵ)
-        ϵ = let ϵ = Arb(1)
-            y = f(ϵ)
-            z = g(ϵ)
+    # Find ϵ2 such that f(ϵ2) satisfies the required tolerance or has
+    # a smaller radius than g(ϵ2)
+    ϵ2 = Arb(1)
+    fϵ2 = abs(f(ϵ2))
+    gϵ2 = abs(g(ϵ2))
+    while !ArbExtras.check_tolerance(fϵ2; rtol) &&
+              !(fϵ2 < ubound_tol) &&
+              radius(fϵ2) > radius(gϵ2)
 
-            # Reduce ϵ until the value we get either satisfies the
-            # required tolerance or is better than the non-asymptotic
-            # version.
-            while !ArbExtras.check_tolerance(y; rtol) && radius(y) > radius(z)
-                ϵ /= 1.2
+        ϵ2 *= 0.8
+        fϵ2 = abs(f(ϵ2))
+        gϵ2 = abs(g(ϵ2))
 
-                y = f(ϵ)
-                z = g(ϵ)
-                if ϵ < 1e-3
-                    @warn "could not determine working ϵ, last tried value" ϵ u0.α
-                    break
-                end
-            end
-            ubound(ϵ)
+        if ϵ2 < 1e-3
+            verbose && @warn "Could not find working ϵ2" ϵ2 fϵ2 gϵ2
+            break
         end
-
-        verbose && @info "ϵ was determined to be" ϵ
     end
 
+    verbose && @info "Determined ϵ2" ϵ2
+
+    # Set tolerances according to currently evaluated values
+    atol = 1.1radius(fϵ2)
+    ubound_tol = max(ubound_tol, ubound(Arb, fϵ2))
+
+    # Find ϵ1 such that f(Arb(0, ϵ1)) satisfies the required tolerance
+    ϵ1 = ϵ2
+    fϵ1 = abs(f(Arb((0, ϵ1))))
+    while !ArbExtras.check_tolerance(fϵ1; rtol, atol) && !(fϵ1 < ubound_tol)
+
+        ϵ1 = ϵ1 < 0.5 ? ϵ1^2 : ϵ1 / 2
+        fϵ1 = abs(f(Arb((0, ϵ1))))
+
+        if ϵ1 < Arb("1e-100000")
+            verbose && @error "Could not find working ϵ1" ϵ1 fϵ1
+            return indeterminate(fϵ1)
+        end
+    end
+
+    verbose && @info "Determined ϵ1" ϵ1
+
     # Bound the value on [0, ϵ]
-    # Estimate the value by evaluating it at ϵ.
-    # Asymptotic evaluation is cheap and we are therefore not too
-    # worried about extra evaluations, the absolute tolerance is
-    # therefore set rather low.
-    estimate = abs(f(Arb(ϵ)))
     max_asymptotic = ArbExtras.maximum_enclosure(
         f,
-        zero(ϵ),
-        ϵ,
+        lbound(ϵ1),
+        ubound(ϵ2),
         abs_value = true,
-        point_value_max = estimate,
-        atol = 1.1radius(estimate); # We can't expect to do much better than this
-        degree,
+        degree = ifelse(u0.use_bhkdv, -1, degree),
+        log_bisection = true,
+        depth = ifelse(u0.use_bhkdv, 40, 20),
+        maxevals = ifelse(u0.use_bhkdv, 50000, 1000);
         rtol,
+        atol,
+        ubound_tol,
         threaded,
         verbose,
     )
 
-    verbose && @info "Bound on [0, ϵ]" max_asymptotic
+    # Take into account enclosure on [0, ϵ1]
+    max_asymptotic = max(max_asymptotic, fϵ1)
+
+    verbose && @info "Bound on [0, ϵ2]" max_asymptotic
+
+    # Ball evaluation is rather expensive and we are worried about
+    # extra evaluations, the tolerances are therefore set rather high.
+    estimate = max(abs(g(ϵ2)), abs(g(Arb(π))))
+    atol = max(max_asymptotic / 2, 2radius(fϵ2))
+    ubound_tol = max(ubound_tol, ubound(Arb, max_asymptotic), ubound(Arb, estimate))
 
     # Bound the value on [ϵ, π] by Ball evaluation
-    # Ball evaluation is rather expensive and we are therefore worried
-    # about extra evaluations, the absolute tolerance is therefore set
-    # a bit higher.
-    estimate = maximum(abs.(g.(range(Arb(ϵ), π, length = 10))))
     max_nonasymptotic = ArbExtras.maximum_enclosure(
         g,
-        ϵ,
+        lbound(ϵ2),
         ubound(Arb(π)),
         abs_value = true,
-        point_value_max = estimate,
-        atol = max(max_asymptotic / 2, 2radius(estimate));
+        depth_start = 2;
         rtol,
+        atol,
+        ubound_tol,
         degree,
         threaded,
         verbose,
     )
 
-    verbose && @info "Bound on [ϵ, π]" max_nonasymptotic
+    verbose && @info "Bound on [ϵ2, π]" max_nonasymptotic
 
     res = max(max_asymptotic, max_nonasymptotic)
     # The result is always nonnegative
