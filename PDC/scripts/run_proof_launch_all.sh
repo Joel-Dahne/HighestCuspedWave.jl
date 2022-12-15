d8a14ad849d38a756c8d6e3e63d4d1f9be9a6340
Joel Dahne <joel@dahne.eu>
2022-12-15 16:25:31 +0100

    PDC: Add start version of script for running all calculations

---
+++ PDC/scripts/run_proof_launch_all.sh
@@ -0,0 +1,25 @@
+# This script launches all calculations required for the proof of the
+# middle chunk of the interval. It launches several Slurm tasks to
+# process different parts of the interval.
+# The numbers given here are currently estimates. They should be
+# updated once a full run has been done.
+
+# 1:1
+# Takes around 44 hours in total. To be conservative the time is set
+# to 48 hours.
+sbatch --time=48:00:00 -p shared -o PDC/logs/run_proof_1.o -e PDC/logs/run_proof_1.e PDC/scripts/run_proof.sh 1 1
+
+# 2:2
+# Takes around 14 hours in total. To be conservative the time is set
+# to 24 hours.
+sbatch --time=24:00:00 -p shared -o PDC/logs/run_proof_2.o -e PDC/logs/run_proof_2.e PDC/scripts/run_proof.sh 2 2
+
+# 3:9
+# Takes around 17 hours in total. To be conservative the time is set
+# to 24 hours.
+sbatch --time=24:00:00 -p shared -o PDC/logs/run_proof_3.o -e PDC/logs/run_proof_3.e PDC/scripts/run_proof.sh 3 9
+
+# 10:end
+# Takes around 16 hours in total. To be conservative the time is set
+# to 24 hours.
+sbatch --time=24:00:00 -p shared -o PDC/logs/run_proof_4.o -e PDC/logs/run_proof_4.e PDC/scripts/run_proof.sh 10 30
