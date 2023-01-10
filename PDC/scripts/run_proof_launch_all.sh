534d600b5abc080de60d91bc05411e39d6504992
Joel Dahne <joel@dahne.eu>
2023-01-10 09:26:08 +0100

    PDC: Update script for interval 1, 2, 3 and 4:10

--- PDC/scripts/run_proof_launch_all.sh
+++ PDC/scripts/run_proof_launch_all.sh
@@ -5,24 +5,36 @@
 # updated once a full run has been done.
 
 # 1:1
-# Takes around 18 hours in total. To be conservative the time is set
-# to 24 hours.
-sbatch --time=24:00:00 -p shared -o PDC/logs/run_proof_1.o -e PDC/logs/run_proof_1.e PDC/scripts/run_proof.sh 1 1
+# Takes around 12 hours in total. To be conservative the time is set
+# to 16 hours.
+# It uses more memory than the other versions and for that reason we
+# use fewer threads to avoid using too much memory. This is slower
+# than if we would have used all threads, but due to SMT it is not
+# that much slower.
+HCW_THREADS=2 sbatch --time=16:00:00 -p main -o PDC/logs/run_proof_1.o -e PDC/logs/run_proof_1.e PDC/scripts/run_proof.sh 1 1
 
 # 2:2
-# Takes around 10 hours in total. To be conservative the time is set
-# to 14 hours.
-sbatch --time=14:00:00 -p shared -o PDC/logs/run_proof_2.o -e PDC/logs/run_proof_2.e PDC/scripts/run_proof.sh 2 2
+# Takes around 9 hours in total. To be conservative the time is set
+# to 12 hours.
+# Similar to the above this uses more memory than the other versions
+# and for that reason we use fewer threads to avoid using too much
+# memory. This is slower than if we would have used all threads, but
+# due to SMT it is not that much slower.
+HCW_THREADS=2 sbatch --time=11:00:00 -p main -o PDC/logs/run_proof_2.o -e PDC/logs/run_proof_2.e PDC/scripts/run_proof.sh 2 2
 
 # 3:3
-# Takes around 8 hours in total. To be conservative the time is set
-# to 12 hours.
-sbatch --time=12:00:00 -p shared -o PDC/logs/run_proof_3.o -e PDC/logs/run_proof_3.e PDC/scripts/run_proof.sh 3 3
+# Takes around 6 hours in total. To be conservative the time is set
+# to 8 hours.
+# Similar to the above this uses more memory than the other versions
+# and for that reason we use fewer threads to avoid using too much
+# memory. This is slower than if we would have used all threads, but
+# due to SMT it is not that much slower.
+HCW_THREADS=2 sbatch --time=8:00:00 -p main -o PDC/logs/run_proof_3.o -e PDC/logs/run_proof_3.e PDC/scripts/run_proof.sh 3 3
 
 # 4:10
-# Takes around 13 hours in total. To be conservative the time is set
-# to 18 hours.
-sbatch --time=18:00:00 -p shared -o PDC/logs/run_proof_4.o -e PDC/logs/run_proof_4.e PDC/scripts/run_proof.sh 4 10
+# Takes around 10 hours in total. To be conservative the time is set
+# to 12 hours.
+sbatch --time=12:00:00 -p main -o PDC/logs/run_proof_4.o -e PDC/logs/run_proof_4.e PDC/scripts/run_proof.sh 4 10
 
 # 11:end
 # Takes around 16 hours in total. To be conservative the time is set
