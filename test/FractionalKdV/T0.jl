c2b158cb1077c1f9b948b72e9b582c8f97e9f52a
Joel Dahne <joel@dahne.eu>
2023-02-22 11:52:54 +0100

    Update tests for FractionalKdV

--- test/FractionalKdV/T0.jl
+++ test/FractionalKdV/T0.jl
@@ -6,9 +6,13 @@
         FractionalKdVAnsatz(add_error(Arb(-0.6), Arb(1e-6))),
         FractionalKdVAnsatz(Arb(-0.3)),
         FractionalKdVAnsatz(add_error(Arb(-0.3), Arb(1e-6))),
+        FractionalKdVAnsatz(Arb(-0.9), use_bhkdv = true),
+        FractionalKdVAnsatz(add_error(Arb(-0.9), Arb(1e-6)), use_bhkdv = true),
+        FractionalKdVAnsatz(Arb(-0.995), use_bhkdv = true),
+        FractionalKdVAnsatz(add_error(Arb(-0.995), Arb(1e-8)), use_bhkdv = true),
     ]
 
-    xs = exp.(range(log(Arb("1e-3")), log(Arb("5e-1")), 10))
+    xs = exp.(range(log(Arb("1e-3")), log(Arb("5e-1")), 5))
 
     for u0 in u0s
         f = T0(u0)
@@ -22,5 +26,7 @@
             # not an enclosure
             @test y1 < y2 || Arblib.overlaps(y1, y2)
         end
+
+        @test Arblib.overlaps(g(Arb((0, xs[1]))), g(xs[1]))
     end
 end
--- test/FractionalKdV/evaluation.jl
+++ test/FractionalKdV/evaluation.jl
@@ -6,9 +6,21 @@
         FractionalKdVAnsatz(add_error(Arb(-0.6), Arb(1e-6))),
         FractionalKdVAnsatz(Arb(-0.3)),
         FractionalKdVAnsatz(add_error(Arb(-0.3), Arb(1e-6))),
+        FractionalKdVAnsatz(Arb(-0.9), use_bhkdv = true),
+        FractionalKdVAnsatz(add_error(Arb(-0.9), Arb(1e-6)), use_bhkdv = true),
+        FractionalKdVAnsatz(Arb(-0.995), use_bhkdv = true),
+        FractionalKdVAnsatz(add_error(Arb(-0.995), Arb(1e-8)), use_bhkdv = true),
+        # These have s values that overlap integers
+        FractionalKdVAnsatz(Arb("[-0.60572740288 +/- 4.35e-11]")),
+        FractionalKdVAnsatz(Arb("[-0.6268228870 +/- 8.17e-11]")),
     ]
 
-    xs = exp.(range(log(Arb("1e-5")), log(Arb("5e-1")), 10))
+    xs = exp.(range(log(Arb("1e-5")), log(Arb("5e-1")), 5))
+    # In theory the asymptotic an non-asymptotic results for ArbSeries
+    # might not overlap. The reason being that the remainder term is
+    # not computed using ArbSeries. In practice they do however
+    # overlap.
+    xs_series = [ArbSeries((x, 1, 0)) for x in xs]
 
     @testset "u0" begin
         for u0 in u0s
@@ -19,6 +31,11 @@
                 @test isfinite(y2)
                 @test Arblib.overlaps(y1, y2)
             end
+
+            @test Arblib.overlaps(
+                u0(Arb((0, xs[1])), Asymptotic()),
+                u0(Arb(xs[1]), Asymptotic()),
+            )
         end
     end
 
@@ -33,6 +50,8 @@
                 @test isfinite(y2)
                 @test Arblib.overlaps(y1, y2)
             end
+
+            @test Arblib.overlaps(g(Arb((0, xs[1]))), g(xs[1]))
         end
     end
 
@@ -40,13 +59,15 @@
         for u0 in u0s
             f = F0(u0)
             g = F0(u0, Asymptotic(), ϵ = Arb("6e-1"))
-            for x in xs
+            for x in ifelse(u0.use_bhkdv, xs, [xs; xs_series])
                 y1 = f(x)
                 y2 = g(x)
                 @test isfinite(y1)
                 @test isfinite(y2)
                 @test Arblib.overlaps(y1, y2)
             end
+
+            @test Arblib.overlaps(g(Arb((0, xs[1]))), g(xs[1]))
         end
     end
 
@@ -54,13 +75,15 @@
         for u0 in u0s
             f = x -> x^(-u0.α) / u0(x)
             g = HighestCuspedWave.inv_u0_normalised(u0)
-            for x in xs
+            for x in [xs; xs_series]
                 y1 = f(x)
                 y2 = g(x)
                 @test isfinite(y1)
                 @test isfinite(y2)
                 @test Arblib.overlaps(y1, y2)
             end
+
+            @test Arblib.overlaps(g(Arb((0, xs[1]))), g(xs[1]))
         end
     end
 end
