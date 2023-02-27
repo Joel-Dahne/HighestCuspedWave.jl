6234c0b7db1a618718b1a4087dc8ff36922a546f
Joel Dahne <joel@dahne.eu>
2023-02-27 10:31:28 +0100

    KdV: Minor updates to D0_bounded_by and its documentation

--- src/FractionalKdV/D0.jl
+++ src/FractionalKdV/D0.jl
@@ -1,8 +1,12 @@
 """
     D0_bounded_by(u0::AbstractAnsatz{Arb}, C::Arf; ϵ = 0, ...)
 
-Attempt to prove that `D₀` is bounded by `C`. Returns `true` on
-success and `false` on failure.
+Attempt to prove that `D₀`, which is the supremum of
+```
+abs(T0(u0)(x))
+```
+for `0 < x < π`, is bounded by `C`. Returns `true` on success and
+`false` on failure.
 
 If `ϵ = 0` it tries to determine an optimal choice of `ϵ` by starting
 with `ϵ = 1` and then iteratively decreasing it until the bound
@@ -15,6 +19,18 @@ It tries to find `ϵ2` such that the bound can be proved with one
 evaluation on ``[0, ϵ2]``. It starts with `ϵ2 = ϵ` and then squares it
 until it succeeds. On the interval ``[ϵ2, ϵ]`` it then uses
 logarithmic bisection.
+
+For the interval ``[ϵ, π]`` we compute `T0(u0)` using the argument
+`skip_div_u0 = true` and then divide the result by `u0(x)` afterwards.
+The reason for doing this is that we can compute a tighter enclosure
+of `u0(x)` in that case.
+
+To efficiently compute a tight enclosure of `u0(x)` we use that it in
+practice is increasing in `x`. We therefore try to prove that it is
+increasing on the interval ``[ϵ, b]``, with `b` slightly smaller than
+`π`, by proving that the derivative is non-negative. This allows us to
+only evaluate it on the endpoints if `x` lies in this interval.
+
 """
 function D0_bounded_by(
     u0::FractionalKdVAnsatz{Arb},
@@ -30,8 +46,6 @@ function D0_bounded_by(
     if iszero(ϵ)
         ϵ = one(ϵ)
         while !(T0(u0, Asymptotic(), ϵ = Arb(1.01ϵ); M)(Arb(ϵ)) < C)
-            # We use the factor 0.8 instead of, say, 0.5 to get
-            # slightly better (higher) values for ϵ.
             ϵ *= 0.8
         end
 
@@ -71,14 +85,12 @@ function D0_bounded_by(
 
     # In practice u0 is increasing on [0, π]. Try to prove that this is
     # the case on the interval [ϵ, b] with b < π.
-    # IMPROVE: Write about this in the documentation.
     b = Arf(3.14)
 
     verbose && @info "Trying to prove that u0 is increasing on [ϵ, b]" b
 
     # Try to prove that minus the derivative of u0 is non-positive,
-    # meaning the derivative is non-negative.
-    # IMPROVE: Compute derivative directly instead of by using
+    # meaning the derivative is non-negative, on [ϵ, b].
     is_increasing = ArbExtras.bounded_by(
         ArbExtras.derivative_function(x -> -u0(x)),
         ϵ,
@@ -108,11 +120,11 @@ function D0_bounded_by(
         isfinite(res) || return res
 
         u0x = if iswide(x)
-            if is_increasing && x < b
-                xₗ, xᵤ = getinterval(Arb, x)
+            if is_increasing && ϵ < x < b
+                xₗ, xᵤ = ArbExtras.enclosure_getinterval(x)
                 # IMPROVE: It would be nice to precompute these so we
                 # don't have two compute all of them twice. The issue
-                # is that they way maximum_enclosure works when degree
+                # is that the way maximum_enclosure works when degree
                 # = -1 makes it so that the right endpoint of one x is
                 # not exactly the same as the left endpoint of the
                 # next x.
